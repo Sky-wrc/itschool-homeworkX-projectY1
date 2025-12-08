b7f84db6eb945f051d198c1bfc2ade70e1f56a38
Sky-WrC <skywarsmike@gmail.com>
2025-12-08 17:43:27 +0300

    добавлены ползунки, буквы, кнопачки, цвет кнопки значение на ползунка теперь связано со значением в textView и варьируется от 0 до 255

--- app/src/main/java/com/example/rgb_palitra/MainActivity.java
+++ app/src/main/java/com/example/rgb_palitra/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rgb_palitra;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,72 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-//
+
+    SeekBar redBar;
+    TextView red;
+    SeekBar greenBar;
+    TextView green;
+    SeekBar blueBar;
+    TextView blue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        redBar = findViewById(R.id.seekBar11);
+        redBar.setMax(255);
+        greenBar = findViewById(R.id.seekBar8);
+        greenBar.setMax(255);
+        blueBar = findViewById(R.id.seekBar2);
+        blueBar.setMax(255);
+        red = findViewById(R.id.textView8);
+        green = findViewById(R.id.textView7);
+        blue = findViewById(R.id.textView6);
+
+
+        red.setText(String.valueOf(redBar.getProgress()));
+        redBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                red.setText(String.valueOf(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
+        green.setText(String.valueOf(greenBar.getProgress()));
+        greenBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                green.setText(String.valueOf(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        blue.setText(String.valueOf(blueBar.getProgress()));
+        blueBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                blue.setText(String.valueOf(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
+
     }
 }
